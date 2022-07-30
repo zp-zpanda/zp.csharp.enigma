46ff5a21f9b8815f643502cbcacd991286be9369
Zhu Panda <zhupandahugo@gmail.com>
2022-07-30 08:40:04 +0800

    add enigma class

---
+++ ZP.CSharp.Enigma/Enigma.cs
@@ -0,0 +1,26 @@
+﻿using System;
+namespace ZP.CSharp.Enigma
+{
+    /**
+    <summary>
+    The base class for the enigma.
+    </summary>
+    */
+    public abstract class Enigma
+    {
+        /**
+        <summary>
+            Runs the enigma on the specified character.
+        </summary>
+        <param name="c">The character to run on.</param>
+        <returns>The resulting character.</returns>
+        <remarks>
+            <para>
+                The enigma can perform encoding and decoding at the same time
+                so there is only one method.
+            </para>
+        </remarks>
+        */
+        public virtual char RunOn(char c) => c;
+    }
+}
