62d70e67b8c9e8bf11a9cdd1bff2972fe991a59d
Zhu Panda <zhupandahugo@gmail.com>
2022-09-11 22:00:26 +0800

    add docs

--- ZP.CSharp.Enigma/Enigma.cs
+++ ZP.CSharp.Enigma/Enigma.cs
@@ -3,11 +3,20 @@ using System.Linq;
 using ZP.CSharp.Enigma;
 namespace ZP.CSharp.Enigma
 {
+    /**
+    <summary>The enigma.</summary>
+    */
     public class Enigma
     {
         public Rotor[] _Rotors;
+        /**
+        <summary>The rotors this enigma has.</summary>
+        */
         public Rotor[] Rotors {get => this._Rotors; set => this._Rotors = value;}
         public Reflector _Reflector;
+        /**
+        <summary>The reflector this enigma has.</summary>
+        */
         public Reflector Reflector {get => this._Reflector; set => this._Reflector = value;}
         public Enigma(Reflector reflector, params Rotor[] rotors)
         {
