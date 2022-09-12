276b310ee421a9913c25bce05fc863d5bd8188af
Zhu Panda <zhupandahugo@gmail.com>
2022-09-12 15:48:58 +0800

    add and refactor tests

--- ZP.CSharp.Enigma.Tests/EnigmaTests.cs
+++ ZP.CSharp.Enigma.Tests/EnigmaTests.cs
@@ -6,11 +6,22 @@ namespace ZP.CSharp.Enigma.Tests
 {
     public class EnigmaTests
     {
-        public Enigma BasicEnigma = new Enigma(new Reflector(new ReflectorPair("ab"), new ReflectorPair("cd")), new Rotor(new RotorPair("ac"), new RotorPair("ba"), new RotorPair("cd"), new RotorPair("db")));
-        [Fact]
-        public void EnigmaWillNotReturnInputAsOutput()
+        public Enigma TestEnigma = new Enigma(new Reflector(new ReflectorPair("ab"), new ReflectorPair("cd")), new Rotor(new RotorPair("ac"), new RotorPair("ba"), new RotorPair("cd"), new RotorPair("db")));
+        [Theory]
+        [InlineData('a')]
+        [InlineData('b')]
+        [InlineData('c')]
+        [InlineData('d')]
+        public void EnigmaWillNotReturnInputAsOutput(char c)
         {
-            Assert.All(new char[]{'a', 'b', 'c', 'd'}, c => Assert.NotEqual(c, this.BasicEnigma.RunOn(c)));
+            Assert.NotEqual(c, this.TestEnigma.RunOn(c));
+        }
+        [Theory]
+        [InlineData("abcd", "cdab")]
+        public void EnigmaWillReturnCipheredOutput(string plain, string cipher)
+        {
+            Assert.Equal(cipher, this.TestEnigma.RunOn(plain));
+            Assert.Equal(plain, this.TestEnigma.RunOn(cipher));
         }
     }
 }
