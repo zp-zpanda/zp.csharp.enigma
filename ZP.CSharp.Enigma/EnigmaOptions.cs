f02833c7d057d168d8c6226aa354a76b3ff8fa92
Zhu Panda <zhupandahugo@gmail.com>
2022-09-18 18:25:31 +0800

    add enum

---
+++ ZP.CSharp.Enigma/EnigmaOptions.cs
@@ -0,0 +1,20 @@
+using System;
+using ZP.CSharp.Enigma;
+namespace ZP.CSharp.Enigma
+{
+    /**
+    <summary>Options for the enigma.</summary>
+    */
+    [Flags]
+    public enum EnigmaOptions
+    {
+        /**
+        <summary>No options are provided.</summary>
+        */
+        Normal = 0,
+        /**
+        <summary>Rotors cannot step.</summary>
+        */
+        NoStepping = 1
+    }
+}
