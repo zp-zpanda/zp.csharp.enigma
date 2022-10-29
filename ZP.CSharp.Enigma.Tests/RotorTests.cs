72cd22500573b193f9176161aea2335858372157
Zhu Panda <zhupandahugo@gmail.com>
2022-10-29 21:36:44 +0800

    change tests

--- ZP.CSharp.Enigma.Tests/RotorTests.cs
+++ ZP.CSharp.Enigma.Tests/RotorTests.cs
@@ -18,7 +18,7 @@ namespace ZP.CSharp.Enigma.Tests
         {
             var pair1 = new RotorPair('a', 'z');
             var pair2 = new RotorPair('z', 'a');
-            var rotor = new Rotor(0, 0, pair1, pair2);
+            var rotor = new Rotor(0, new[]{0}, pair1, pair2);
             Assert.Contains(pair1, rotor.Pairs);
             Assert.Contains(pair2, rotor.Pairs);
         }
@@ -27,7 +27,7 @@ namespace ZP.CSharp.Enigma.Tests
         [InlineData("你我他大熊貓", "大熊貓你我他", new char[]{'你', '我', '他', '大', '熊', '貓'}, new char[]{'大', '熊', '貓', '你', '我', '他'})]
         public void RotorPairsCanBeMassConstructedFromTwoMappings(string e, string r, char[] eChars, char[] rChars)
         {
-            var rotor = new Rotor(0, 0, e, r);
+            var rotor = new Rotor(0, new[]{0}, e, r);
             var i = 0;
             Assert.All(rotor.Pairs, pair => {
                 Assert.Equal(pair, new RotorPair(eChars[i], rChars[i]));
@@ -39,7 +39,7 @@ namespace ZP.CSharp.Enigma.Tests
         [InlineData(new char[]{'你', '我', '他', '大', '熊', '貓'}, new char[]{'大', '熊', '貓', '你', '我', '他'}, "你大", "我熊", "他貓", "大你", "熊我", "貓他")]
         public void RotorPairsCanBeMassConstructedFromTwoCharLongMappings(char[] eChars, char[] rChars, params string[] maps)
         {
-            var rotor = new Rotor(0, 0, maps);
+            var rotor = new Rotor(0, new[]{0}, maps);
             var i = 0;
             Assert.All(rotor.Pairs, pair => {
                 Assert.Equal(pair, new RotorPair(eChars[i], rChars[i]));
@@ -61,7 +61,7 @@ namespace ZP.CSharp.Enigma.Tests
         [InlineData(false, false, new[]{"aacd", "abcc"})]
         public void RotorCanBeValidated(bool twoStrings, bool isValid, string[] maps)
         {
-            var action = () => {var rotor = twoStrings ? new Rotor(0, 0, maps) : new Rotor(0, 0, maps[0], maps[1]);};
+            var action = () => {var rotor = twoStrings ? new Rotor(0, new[]{0}, maps) : new Rotor(0, new[]{0}, maps[0], maps[1]);};
             if (isValid)
             {
                 action();
@@ -79,7 +79,7 @@ namespace ZP.CSharp.Enigma.Tests
         [InlineData("大熊貓可愛", "可愛熊貓大", '人', null)]
         public void RotorCanPassCharacterFromEntryWheel(string e, string r, char input, char? expected)
         {
-            var action = () => Assert.Equal(expected, new Rotor(0, 0, e, r).FromEntryWheel(input));
+            var action = () => Assert.Equal(expected, new Rotor(0, new[]{0}, e, r).FromEntryWheel(input));
             if (expected is not null)
             {
                 action();
@@ -97,7 +97,7 @@ namespace ZP.CSharp.Enigma.Tests
         [InlineData("可愛熊貓大", "大熊貓可愛", '人', null)]
         public void RotorCanPassCharacterFromReflector(string e, string r, char input, char? expected)
         {
-            var action = () => Assert.Equal(expected, new Rotor(0, 0, e, r).FromReflector(input));
+            var action = () => Assert.Equal(expected, new Rotor(0, new[]{0}, e, r).FromReflector(input));
             if (expected is not null)
             {
                 action();
@@ -112,7 +112,7 @@ namespace ZP.CSharp.Enigma.Tests
         [InlineData("abcde", "edcba", 5, 'a', new[]{'e', 'c', 'a', 'd', 'b'})]
         public void RotorCanRotate(string e, string r, int total, char eChar, char[] rArr)
         {
-            var rotor = new Rotor(0, 0, e, r);
+            var rotor = new Rotor(0, new[]{0}, e, r);
             for (int i = 0; i < total; i++)
             {
                 Assert.Equal(rArr[i], rotor.FromEntryWheel(eChar));
@@ -125,7 +125,7 @@ namespace ZP.CSharp.Enigma.Tests
         [InlineData("abcde", "edcba", 4, 'a', 'b')]
         public void RotorCanRotateCharacterBasingOnPosition(string e, string r, int pos, char eChar, char rChar)
         {
-            var rotor = new Rotor(pos, 0, e, r);
+            var rotor = new Rotor(pos, new[]{0}, e, r);
             Assert.Equal(rChar, rotor.FromEntryWheel(eChar));
             Assert.Equal(eChar, rotor.FromReflector(rChar));
         }
@@ -136,7 +136,7 @@ namespace ZP.CSharp.Enigma.Tests
         public void RotorCanAllowNextToStep(int total, int notch)
         {
             var map = new string(Enumerable.Range('a', total).Select(i => (char) i).ToArray());
-            var rotor = new Rotor(0, notch, map, map);
+            var rotor = new Rotor(0, new[]{notch}, map, map);
             for (int i = 0; i < total; i++)
             {
                 Assert.Equal(i == notch, rotor.AllowNextToStep());
