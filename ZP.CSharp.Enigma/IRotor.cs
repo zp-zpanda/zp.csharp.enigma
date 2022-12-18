eb00fb7e8e11618ee34ec44a3b7ef729b8b7c89c
Zhu Panda <zhupandahugo@gmail.com>
2022-12-18 20:12:25 +0800

    rewrite methods as default interface implementations

--- ZP.CSharp.Enigma/IRotor.cs
+++ ZP.CSharp.Enigma/IRotor.cs
@@ -7,8 +7,11 @@ namespace ZP.CSharp.Enigma
     /**
     <summary>The interface for the rotor.</summary>
     */
-    public interface IRotor
+    public interface IRotor<TRotor, TRotorPair>
+        where TRotor : IRotor<TRotor, TRotorPair>
+        where TRotorPair : IRotorPair<TRotorPair>
     {
+        
         /**
         <summary>The domain of this rotor.</summary>
         */
@@ -21,58 +24,6 @@ namespace ZP.CSharp.Enigma
         <summary>The turning notches of this rotor.</summary>
         */
         public int[] Notch {get; set;}
-        /**
-        <summary>Checks if the rotor is in a valid state, in which it is bijective (i.e. one-to-one, fully invertible).</summary>
-        <returns><c><see langword="true" /></c> if valid, else <c><see langword="false" /></c>.</returns>
-        */
-        public bool IsValid();
-        /**
-        <summary>Matches a character from the entry wheel.</summary>
-        <param name="c">The character to map.</param>
-        <returns>The mapped character.</returns>
-        <exception cref="CharacterNotFoundException"><paramref name="c" /> cannot be mapped.</exception>
-        */
-        public char FromEntryWheel(char c);
-        /**
-        <summary>Matches a character from the reflector.</summary>
-        <param name="c">The character to map.</param>
-        <returns>The mapped character.</returns>
-        <exception cref="CharacterNotFoundException"><paramref name="c" /> cannot be mapped.</exception>
-        */
-        public char FromReflector(char c);
-        /**
-        <summary>Computes the domain of this rotor.</summary>
-        <returns>The domain.</returns>
-        */
-        public string ComputeDomain();
-        /**
-        <summary>Transposes a character coming to the rotor.</summary>
-        <param name="c">The character to transpose.</param>
-        <returns>The transposed character.</returns>
-        */
-        public char TransposeIn(char c);
-        /**
-        <summary>Transposes a character going away from the rotor.</summary>
-        <param name="c">The character to transpose.</param>
-        <returns>The transposed character.</returns>
-        */
-        public char TransposeOut(char c);
-        /**
-        <summary>Returns whether this rotor allows the next to step or not.</summary>
-        */
-        public bool AllowNextToStep();
-        /**
-        <summary>Steps the rotor.</summary>
-        */
-        public void Step();
-    }
-    /**
-    <summary>The generic interface for the rotor.</summary>
-    */
-    public interface IRotor<TRotor, TRotorPair> : IRotor
-        where TRotor : IRotor<TRotor, TRotorPair>, IRotor
-        where TRotorPair : IRotorPair<TRotorPair>
-    {
         /**
         <summary>The rotor pairs this rotor has.</summary>
         */
@@ -99,6 +50,87 @@ namespace ZP.CSharp.Enigma
         <param name="r">The reflector-side mapping.</param>
         */
         public static abstract TRotor WithPositionNotchAndTwoMaps(int pos, int[] notch, string e, string r);
-
+        /**
+        <summary>Checks if the rotor is in a valid state, in which it is bijective (i.e. one-to-one, fully invertible).</summary>
+        <returns><c><see langword="true" /></c> if valid, else <c><see langword="false" /></c>.</returns>
+        */
+        public bool IsValid()
+        {
+            var eArr = this.Pairs.Select(pair => pair.Map.EntryWheelSide).OrderBy(e => e);
+            var rArr = this.Pairs.Select(pair => pair.Map.ReflectorSide).OrderBy(r => r);
+            var e = !eArr.GroupBy(e => e).Select(group => group.Count()).Any(count => count > 1);
+            var r = !rArr.GroupBy(r => r).Select(group => group.Count()).Any(count => count > 1);
+            var same = eArr.SequenceEqual(rArr);
+            return (e && r && same);
+        }
+        /**
+        <summary>Matches a character from the entry wheel.</summary>
+        <param name="c">The character to map.</param>
+        <returns>The mapped character.</returns>
+        <exception cref="CharacterNotFoundException"><paramref name="c" /> cannot be mapped.</exception>
+        */
+        public char FromEntryWheel(char c)
+        {
+            try
+            {
+                return this.TransposeOut(this.Pairs.Where(pair => this.TransposeIn(c) == pair.Map.EntryWheelSide).Single().Map.ReflectorSide);
+            }
+            catch
+            {
+                throw new CharacterNotFoundException();
+            }
+        }
+        /**
+        <summary>Matches a character from the reflector.</summary>
+        <param name="c">The character to map.</param>
+        <returns>The mapped character.</returns>
+        <exception cref="CharacterNotFoundException"><paramref name="c" /> cannot be mapped.</exception>
+        */
+        public char FromReflector(char c)
+        {
+            try
+            {
+                return this.TransposeOut(this.Pairs.Where(pair => this.TransposeIn(c) == pair.Map.ReflectorSide).Single().Map.EntryWheelSide);
+            }
+            catch
+            {
+                throw new CharacterNotFoundException();
+            }
+        }
+        /**
+        <summary>Computes the domain of this rotor.</summary>
+        <returns>The domain.</returns>
+        */
+        public virtual string ComputeDomain() => new string(this.Pairs.Select(pair => pair.Map.EntryWheelSide).ToArray());
+        /**
+        <summary>Transposes a character coming to the rotor.</summary>
+        <param name="c">The character to transpose.</param>
+        <returns>The transposed character.</returns>
+        */
+        public char TransposeIn(char c)
+        {
+            var index = this.Domain.IndexOf(c);
+            var length = this.Domain.Length;
+            return this.Domain[(index + this.Position) % length];
+        }
+        /**
+        <summary>Transposes a character going away from the rotor.</summary>
+        <param name="c">The character to transpose.</param>
+        <returns>The transposed character.</returns>
+        */
+        public char TransposeOut(char c)
+        {
+            var index = this.Domain.IndexOf(c);
+            var length = this.Domain.Length;
+            return this.Domain[(index - this.Position + length) % length];
+        }
+        /**
+        <summary>Returns whether this rotor allows the next to step or not.</summary>
+        */
+        public bool AllowNextToStep();
+        /**
+        <summary>Steps the rotor.</summary>
+        */
+        public void Step();
     }
 }
