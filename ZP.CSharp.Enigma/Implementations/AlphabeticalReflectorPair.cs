88c45600e50bd372324acf3cf6b37e821435d998
Zhu Panda <zhupandahugo@gmail.com>
2022-12-28 14:31:36 +0800

    decouple reflector pair types

---
+++ ZP.CSharp.Enigma/Implementations/AlphabeticalReflectorPair.cs
@@ -0,0 +1,103 @@
+using System;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using ZP.CSharp.Enigma.Implementations;
+namespace ZP.CSharp.Enigma.Implementations
+{
+    /**
+    <summary>The reflector pair.</summary>
+    */
+    public class AlphabeticalReflectorPair : IReflectorPair<AlphabeticalReflectorPair>, IEquatable<AlphabeticalReflectorPair>
+    {
+        private (char One, char Two) _Map;
+        /**
+        <summary>The mapping.</summary>
+        */
+        public required (char One, char Two) Map {get => this._Map; set => this._Map = value;}
+        /**
+        <inheritdoc cref="ReflectorPair.WithTwoCharacters(char, char)" />
+        */
+        [SetsRequiredMembers]
+        protected AlphabeticalReflectorPair(char one, char two)
+        {
+            if (one == two)
+            {
+                throw new ArgumentException("Reflector must have two different characters to map to.");
+            }
+            var map = new[]{one, two}.OrderBy(c => c);
+            this.Map = (map.First(), map.Last());
+        }
+        /**
+        <inheritdoc cref="IReflectorPair{TReflectorPair}.WithTwoCharacters(char, char)" />
+        */
+        public static AlphabeticalReflectorPair WithTwoCharacters(char one, char two) => new AlphabeticalReflectorPair(one, two);
+        /**
+        <inheritdoc cref="ReflectorPair.WithMap(string)" />
+        */
+        [SetsRequiredMembers]
+        protected AlphabeticalReflectorPair(string map)
+        {
+            if (map.Length != 2)
+            {
+                throw new ArgumentException("Mapping is not two characters long. Expected mapping: \"{One}{Two}\"");
+            }
+            if (map[0] == map[1])
+            {
+                throw new ArgumentException("Reflector must have two different characters to map to.");
+            }
+            var mapArr = map.ToCharArray().OrderBy(c => c);
+            this.Map = (mapArr.First(), mapArr.Last());
+        }
+        /**
+        <inheritdoc cref="IReflectorPair{TReflectorPair}.WithMap(string)" />
+        */
+        public static AlphabeticalReflectorPair WithMap(string map) => new AlphabeticalReflectorPair(map);
+        /**
+        <summary>Produces the hash code for the reflector pair.</summary>
+        */
+        public override int GetHashCode() => this.Map.GetHashCode();
+        /**
+        <summary>Checks reflector pair equality.</summary>
+        <param name="pair">The reflector pair to compare to.</param>
+        <returns>The result.</returns>
+        <seealso cref="Equals(object?)" />
+        <seealso cref="operator ==" />
+        <seealso cref="operator !=" />
+        */
+        public bool Equals(AlphabeticalReflectorPair? pair)
+        {
+            if (pair is null)
+            {
+                return false;
+            }
+            return pair.Map == this.Map;
+        }
+        /**
+        <inheritdoc cref="Equals(AlphabeticalReflectorPair?)" />
+        <param name="obj">The object to compare to.</param>
+        <seealso cref="Equals(AlphabeticalReflectorPair?)" />
+        <seealso cref="operator ==" />
+        <seealso cref="operator !=" />
+        */
+        public override bool Equals(object? obj) => this.Equals(obj as ReflectorPair);
+        /**
+        <inheritdoc cref="Equals(AlphabeticalReflectorPair?)" />
+        <param name="pair1">Reflector pair 1.</param>
+        <param name="pair2">Reflector pair 2.</param>
+        <seealso cref="Equals(object?)" />
+        <seealso cref="Equals(AlphabeticalReflectorPair?)" />
+        <seealso cref="operator !=" />
+        */
+        public static bool operator ==(AlphabeticalReflectorPair pair1, AlphabeticalReflectorPair pair2) => pair1.Equals(pair2);
+        /**
+        <inheritdoc cref="Equals(AlphabeticalReflectorPair?)" />
+        <summary>Checks rotor pair inequality.</summary>
+        <param name="pair1">Reflector pair 1.</param>
+        <param name="pair2">Reflector pair 2.</param>
+        <seealso cref="Equals(object?)" />
+        <seealso cref="Equals(AlphabeticalReflectorPair?)" />
+        <seealso cref="operator ==" />
+        */
+        public static bool operator !=(AlphabeticalReflectorPair pair1, AlphabeticalReflectorPair pair2) => !pair1.Equals(pair2);
+    }
+}
