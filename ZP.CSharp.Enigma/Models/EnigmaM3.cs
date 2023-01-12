08c644eedede62826c5b321556f4eac447196983
Z Panda <zhupandahugo@gmail.com>
2023-01-12 21:16:07 +0800

    refactor xml comments

--- ZP.CSharp.Enigma/Models/EnigmaM3.cs
+++ ZP.CSharp.Enigma/Models/EnigmaM3.cs
@@ -29,43 +29,43 @@ namespace ZP.CSharp.Enigma.Models
         */
         public AlphabeticalReflector Reflector {get => this._Reflector; set => this._Reflector = value;}
         /**
-        <summary> M3 rotor I.</summary>
+        <summary>M3 rotor I.</summary>
         */
         public static AlphabeticalRotor I {get => AlphabeticalRotor.New(0, new[]{16}, "EKMFLGDQVZNTOWYHXUSPAIBRCJ");}
         /**
-        <summary> M3 rotor II.</summary>
+        <summary>M3 rotor II.</summary>
         */
         public static AlphabeticalRotor II {get => AlphabeticalRotor.New(0, new[]{4}, "AJDKSIRUXBLHWTMCQGZNPYFVOE");}
         /**
-        <summary> M3 rotor III.</summary>
+        <summary>M3 rotor III.</summary>
         */
         public static AlphabeticalRotor III {get => AlphabeticalRotor.New(0, new[]{21}, "BDFHJLCPRTXVZNYEIWGAKMUSQO");}
         /**
-        <summary> M3 rotor IV.</summary>
+        <summary>M3 rotor IV.</summary>
         */
         public static AlphabeticalRotor IV {get => AlphabeticalRotor.New(0, new[]{9}, "ESOVPZJAYQUIRHXLNFTGKDCMWB");}
         /**
-        <summary> M3 rotor V.</summary>
+        <summary>M3 rotor V.</summary>
         */
         public static AlphabeticalRotor V {get => AlphabeticalRotor.New(0, new[]{25}, "VZBRGITYUPSDNHLXAWMJQOFECK");}
         /**
-        <summary> M3 rotor VI.</summary>
+        <summary>M3 rotor VI.</summary>
         */
         public static AlphabeticalRotor VI {get => AlphabeticalRotor.New(0, new[]{25, 12}, "JPGVOUMFYQBENHZRDKASXLICTW");}
         /**
-        <summary> M3 rotor VII.</summary>
+        <summary>M3 rotor VII.</summary>
         */
         public static AlphabeticalRotor VII {get => AlphabeticalRotor.New(0, new[]{25, 12}, "NZJHGRCXMYSWBOUFAIVLPEKQDT");}
         /**
-        <summary> M3 rotor VIII.</summary>
+        <summary>M3 rotor VIII.</summary>
         */
         public static AlphabeticalRotor VIII {get => AlphabeticalRotor.New(0, new[]{25, 12}, "FKQHTLXOCBJSPDZRAMEWNIUYGV");}
         /**
-        <summary> M3 reflector B.</summary>
+        <summary>M3 reflector B.</summary>
         */
         public static AlphabeticalReflector B {get => AlphabeticalReflector.New("YARBUCHDQESFLGPIXJNKOMZTWV");}
         /**
-        <summary> M3 reflector C.</summary>
+        <summary>M3 reflector C.</summary>
         */
         public static AlphabeticalReflector C {get => AlphabeticalReflector.New("FAVBPCJDIEOGYHRKZLXMWNTQUS");}
         /**
