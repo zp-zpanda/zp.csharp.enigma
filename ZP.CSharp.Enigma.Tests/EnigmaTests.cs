c0523e4b2c3499d56d30e50b4b391dd4a0c150c4
Z Panda <zhupandahugo@gmail.com>
2023-01-21 13:47:38 +0800

    add tests

--- ZP.CSharp.Enigma.Tests/EnigmaTests.cs
+++ ZP.CSharp.Enigma.Tests/EnigmaTests.cs
@@ -1,34 +1,35 @@
+using System.Collections.Generic;
 using Xunit;
 using ZP.CSharp.Enigma.Helpers;
 namespace ZP.CSharp.Enigma.Tests
 {
     public class EnigmaTests
     {
-        public Enigma TestEnigma = Enigma.New(
-            Entrywheel.New(
-                EntrywheelPair.New("aa"),
-                EntrywheelPair.New("bb"),
-                EntrywheelPair.New("cc"),
-                EntrywheelPair.New("dd")
+        public Enigma<IEnumerable<int>, int> TestEnigma = Enigma<IEnumerable<int>, int>.New(
+            Entrywheel<int>.New(
+                EntrywheelPair<int>.New(0, 0),
+                EntrywheelPair<int>.New(1, 1),
+                EntrywheelPair<int>.New(2, 2),
+                EntrywheelPair<int>.New(3, 3)
             ),
-            Reflector.New(
-                ReflectorPair.New("ab"),
-                ReflectorPair.New("cd")
+            Reflector<int>.New(
+                ReflectorPair<int>.New(0, 1),
+                ReflectorPair<int>.New(2, 3)
             ),
-            Rotor.New(
+            Rotor<int>.New(
                 0,
                 new[]{0},
-                RotorPair.New("ac"),
-                RotorPair.New("ba"),
-                RotorPair.New("cd"),
-                RotorPair.New("db")
+                RotorPair<int>.New(0, 2),
+                RotorPair<int>.New(1, 0),
+                RotorPair<int>.New(2, 3),
+                RotorPair<int>.New(3, 1)
             )
         );
         [Theory]
-        [InlineData('a')]
-        [InlineData('b')]
-        [InlineData('c')]
-        [InlineData('d')]
+        [InlineData(0)]
+        [InlineData(1)]
+        [InlineData(2)]
+        [InlineData(3)]
         public void EnigmaWillNotReturnInputAsOutput(char c)
         {
             var enigma = this.TestEnigma;
@@ -36,8 +37,8 @@ namespace ZP.CSharp.Enigma.Tests
             Assert.NotEqual(c, result);
         }
         [Theory]
-        [InlineData("abcd", "cdab")]
-        public void EnigmaWillReturnCipheredOutput(string plain, string cipher)
+        [InlineData(new[]{0, 1, 2, 3}, new[]{2, 3, 0, 1})]
+        public void EnigmaWillReturnCipheredOutput(int[] plain, int[] cipher)
         {
             var enigma = this.TestEnigma;
             var result = enigma.RunOn(plain);
