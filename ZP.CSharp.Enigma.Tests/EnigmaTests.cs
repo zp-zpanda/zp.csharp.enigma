bfbe1b864fc7565db4fb4bc4852fb34ea44d0657
Z Panda <zhupandahugo@gmail.com>
2023-02-12 18:15:43 +0800

    refactor to static property

--- ZP.CSharp.Enigma.Tests/EnigmaTests.cs
+++ ZP.CSharp.Enigma.Tests/EnigmaTests.cs
@@ -5,7 +5,7 @@ namespace ZP.CSharp.Enigma.Tests
 {
     public class EnigmaTests
     {
-        public Enigma<IEnumerable<int>, int> TestEnigma = Enigma<IEnumerable<int>, int>.New(
+        public static Enigma<IEnumerable<int>, int> TestEnigma => Enigma<IEnumerable<int>, int>.New(
             Entrywheel<int>.New(
                 EntrywheelPair<int>.New(0, 0),
                 EntrywheelPair<int>.New(1, 1),
@@ -32,7 +32,7 @@ namespace ZP.CSharp.Enigma.Tests
         [InlineData(3)]
         public void EnigmaWillNotReturnInputAsOutput(char c)
         {
-            var enigma = this.TestEnigma;
+            var enigma = TestEnigma;
             var result = enigma.RunOn(c);
             Assert.NotEqual(c, result);
         }
@@ -40,7 +40,7 @@ namespace ZP.CSharp.Enigma.Tests
         [InlineData(new[]{0, 1, 2, 3}, new[]{2, 3, 0, 1})]
         public void EnigmaWillReturnCipheredOutput(int[] plain, int[] cipher)
         {
-            var enigma = this.TestEnigma;
+            var enigma = TestEnigma;
             var result = enigma.RunOn(plain);
             Assert.Equal(cipher, result);
         }
